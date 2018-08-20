061faabb3e2d20bda493b841fdd3bd84f8993df7
live-beta <wanjala.sam.s@gmail.com>
2018-08-20 15:42:47 +0300

    --amend

--- app/src/androidTest/java/com/example/sam/moviebox/TestNetworkLayer.java
+++ app/src/androidTest/java/com/example/sam/moviebox/TestNetworkLayer.java
@@ -15,9 +15,7 @@ import java.io.IOException;
 import static org.junit.Assert.*;
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Unit tests for network connection
  */
 @RunWith(AndroidJUnit4.class)
 public class TestNetworkLayer {
