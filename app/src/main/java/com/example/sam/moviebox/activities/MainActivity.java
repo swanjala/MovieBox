e424c49985468462044a0490667e466591358bf0
live-beta <wanjala.sam.s@gmail.com>
2018-08-27 10:47:57 +0300

    --amend

--- app/src/main/java/com/example/sam/moviebox/activities/MainActivity.java
+++ app/src/main/java/com/example/sam/moviebox/activities/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
 import com.example.sam.moviebox.R;
 import com.example.sam.moviebox.networkUtils.INetworkCalls;
 import com.example.sam.moviebox.networkUtils.NetworkCalls;
@@ -14,13 +16,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     RecyclerView.Adapter mAdapter;
     RecyclerView.LayoutManager mLayoutManager;
-    JSONArray jsonArray;
+    JSONArray jsonArray,genreDataArray;
 
 
     @Override
@@ -40,10 +43,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class dataCallTask extends AsyncTask<Context, Void, JSONArray> {
+    public class dataCallTask extends AsyncTask<Context, Void, ArrayList<JSONArray>> {
 
         @Override
-        protected JSONArray doInBackground(Context... contexts) {
+        protected ArrayList<JSONArray> doInBackground(Context... contexts) {
+
+            ArrayList<JSONArray> networkCallResults = new ArrayList<>();
+
             final INetworkCalls networkCalls = new NetworkCalls(getApplicationContext());
 
             try {
@@ -53,15 +59,31 @@ public class MainActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            return jsonArray;
+            try{
+                genreDataArray = networkCalls.genreResults();
+            }catch (IOException e){
+                e.printStackTrace();
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+            networkCallResults.add(jsonArray);
+            networkCallResults.add(genreDataArray);
+
+            return networkCallResults;
         }
 
         @Override
-        protected void onPostExecute(JSONArray jsonArray) {
+        protected void onPostExecute(ArrayList<JSONArray> jsonArrayList) {
             if (jsonArray != null) {
 
-                mAdapter = new MainRecyclerAdapter(getApplicationContext(), jsonArray);
+                Log.d("genre data", String.valueOf(jsonArrayList.get(1)));
+
+                mAdapter = new MainRecyclerAdapter(getApplicationContext()
+                        ,jsonArrayList.get(0)
+                        ,jsonArrayList.get(1));
                 mRecyclerView.setAdapter(mAdapter);
+
+
             }
         }
     }
