c4ddc62557c7370cf3ddefd4181e446adde79201
swanjala <wanjala.sam.s@gmail.com>
2019-02-18 13:59:40 +0300

    (add) Fragment details

--- app/src/main/java/com/example/sam/moviebox/views/fragments/FragmentDetails.java
+++ app/src/main/java/com/example/sam/moviebox/views/fragments/FragmentDetails.java
@@ -1,6 +1,54 @@
 package com.example.sam.moviebox.views.fragments;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class FragmentDetails extends Fragment {
+import com.example.sam.moviebox.R;
+
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
+
+public class FragmentDetails extends Fragment implements Injectable{
+
+    @Inject
+    public ViewModelProviders.Factory mViewModelFactory;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        postponeEnterTransition();
+
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_movie_details,container,false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+    public static FragmentDetails create(int movieId){
+        FragmentDetails fragmentDetails = new FragmentDetails();
+        Bundle args = new Bundle();
+        args.putInt(BUNDLE_MOVIE_ID, movieId);
+        fragmentDetails.setArguments(args);
+        return fragmentDetails;
+
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        // add details
+    }
+    // add response handlers
+
 }
