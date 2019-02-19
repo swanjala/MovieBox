394501cb2c2086821f63c0037a76ea5139753ccc
swanjala <wanjala.sam.s@gmail.com>
2019-02-19 09:04:29 +0300

    (add): moviebox retrofit service
    - Add interface with api routes and call functions

--- app/src/main/java/com/example/sam/moviebox/repository/api/MovieBoxService.java
+++ app/src/main/java/com/example/sam/moviebox/repository/api/MovieBoxService.java
@@ -1,4 +1,16 @@
-package com.example.sam.moviebox.repository;
+package com.example.sam.moviebox.repository.api;
 
-public class MovieBoxService {
+import android.arch.lifecycle.LiveData;
+
+import com.example.sam.moviebox.repository.model.ApiResponse;
+
+import retrofit2.http.GET;
+
+public interface MovieBoxService {
+
+    @GET("movie/popular?")
+    LiveData<ApiResponse<MovieResponse>> getPopularMovies();
+
+    @GET("genre/movie/list")
+    LiveData<ApiResponse<GenreResponse>> getGenres();
 }
