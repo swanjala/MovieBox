11d75fe94374acf40b39b99b0f699177054a2c7d
swanjala <wanjala.sam.s@gmail.com>
2019-02-21 16:34:29 +0300

    (refactor): app component class
    - Add dependency imports

--- app/src/main/java/com/example/sam/moviebox/dependencies/component/AppComponent.java
+++ app/src/main/java/com/example/sam/moviebox/dependencies/component/AppComponent.java
@@ -1,6 +1,13 @@
 package com.example.sam.moviebox.dependencies.component;
 
 
+import android.app.Application;
+
+import com.example.sam.moviebox.MovieBoxApp;
+import com.example.sam.moviebox.dependencies.module.AppModule;
+import com.example.sam.moviebox.dependencies.module.MainActivityModule;
+import com.example.sam.moviebox.dependencies.module.RoomModule;
+
 import javax.inject.Singleton;
 
 import dagger.BindsInstance;
