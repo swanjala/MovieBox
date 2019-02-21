c697ae8f64f9b37c409fbecae71d0545f6805c89
swanjala <wanjala.sam.s@gmail.com>
2019-02-21 17:44:19 +0300

    (add): application constants

--- app/src/main/java/com/example/sam/moviebox/utils/Constants.java
+++ app/src/main/java/com/example/sam/moviebox/utils/Constants.java
@@ -1,4 +1,27 @@
 package com.example.sam.moviebox.utils;
 
 public class Constants {
+
+    public static boolean DEBUG = true;
+    /**
+     * Base Url (Endpoint)
+     */
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+
+    /**
+     * Connection read timeout
+     */
+    public static final int READ_TIMEOUT = 60 * 1000;
+
+    /**
+     * Connection write timeout
+     */
+    public static final int WRITE_TIMEOUT = 60 * 1000;
+
+    /**
+     * Connection timeout
+     */
+    public static final int CONNECT_TIMEOUT = 60 * 1000;
+
+
 }
