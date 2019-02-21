92539acd34ccebc0a416ab6439bc1dc6593c83f8
swanjala <wanjala.sam.s@gmail.com>
2019-02-21 16:52:12 +0300

    (Refactor): test app
    - Implement creation of an instance
    - Remove unused code

--- app/src/androidTest/java/com/example/sam/moviebox/TestApp.java
+++ app/src/androidTest/java/com/example/sam/moviebox/TestApp.java
@@ -1,42 +1,12 @@
 package com.example.sam.moviebox;
 
-import android.content.Context;
-import android.os.AsyncTask;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import android.app.Application;
 
-import com.example.sam.moviebox.classInterfaces.INetworkCalls;
-import com.example.sam.moviebox.networkUtils.NetworkCalls;
+public class TestApp extends Application {
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-/**
- * Unit tests for network connection
- */
-@RunWith(AndroidJUnit4.class)
-public class TestNetworkLayer {
-
-    private Context appContext = InstrumentationRegistry.getTargetContext();
-
-    @Test
-    public void useAppContext() {
-
-        assertEquals("com.example.sam.moviebox", appContext.getPackageName());
-    }
-
-    @Test
-    public void testAppGetsDataFromNetwork() throws IOException, JSONException, Throwable {
-
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
     }
 
