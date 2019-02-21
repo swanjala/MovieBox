3252de026126dab4662c6797a0049280f21466ef
swanjala <wanjala.sam.s@gmail.com>
2019-02-21 16:30:49 +0300

    (refactor): update database class
    - Refactor code with abstract declaration
    - Remove unused code

--- app/src/main/java/com/example/sam/moviebox/database/MovieBoxDatabase.java
+++ app/src/main/java/com/example/sam/moviebox/database/MovieBoxDatabase.java
@@ -1,41 +1,19 @@
 package com.example.sam.moviebox.database;
 
 import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
-import android.arch.persistence.room.TypeConverters;
-import android.content.Context;
-import android.util.Log;
 
+import com.example.sam.moviebox.database.dao.GenreDao;
 import com.example.sam.moviebox.database.dao.MovieDao;
+import com.example.sam.moviebox.model.GenreModel;
 import com.example.sam.moviebox.model.MovieModel;
 
-@Database(entities = {MovieModel.class}, version = 31, exportSchema = false)
-@TypeConverters(JSONConverter.class)
-public abstract class MovieDatabase extends RoomDatabase{
+@Database(entities = {MovieModel.class, GenreModel.class}, version = 37)
+public abstract class MovieBoxDatabase extends RoomDatabase {
 
-    private static final String LOG_TAG = MovieDatabase.class.getSimpleName();
-    private static final Object LOCK = new Object();
-    private static final String DATABASE_NAME = "moviebox";
-    private static MovieDatabase movieInstance;
+     abstract public MovieDao movieDao();
 
-    public  static MovieDatabase getMovieInstance(Context context) {
-
-            if (movieInstance == null) {
-                synchronized (LOCK) {
-                    Log.d(LOG_TAG, "Creating new database instance");
-                    movieInstance = Room.databaseBuilder(context.getApplicationContext(),
-                            MovieDatabase.class, MovieDatabase.DATABASE_NAME)
-                            .fallbackToDestructiveMigration()
-                            .build();
-                }
-            }
-            Log.d(LOG_TAG, "Getting the database Instance");
-            return movieInstance;
-
-    }
-
-    public abstract MovieDao movieDao();
+     abstract public GenreDao genreDao();
 
 
 }
